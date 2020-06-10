5fb9aef9f3b845f5f2c9e04dbf11cc39155605b5
Marc Mintel <marc@mintel.me>
2020-06-10 12:05:05 +0700

    json service tests

--- server/src/json/json.service.ts
+++ server/src/json/json.service.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs-extra';
 import fg from 'fast-glob'
+import { extname } from 'path';
 import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
@@ -8,7 +9,13 @@ export class JsonService {
 
   async readFile<T>(path: string): Promise<T> {
     this.logger.verbose(`read file: ${path}`);
-    return fs.readJson(path);
+    const json = this.ensureJson(path);
+    try {
+      return await fs.readJson(json);
+    } catch {
+      this.logger.warn(`could not read file: ${path}`)
+      return null;
+    }
   }
 
   async readDir<T>(path: string): Promise<Array<T>> {
@@ -17,5 +24,19 @@ export class JsonService {
     return Promise.all(dir.map(async (fileName: string) => {
       return this.readFile<any>(fileName)
     }))
+  }
+
+  private ensureJson(path:string) {
+    let jsonPath = path;
+    const json = '.json';
+    const ext = extname(path);
+
+    if (ext === '') {
+      jsonPath = `${path}${json}`;
+    } else if (ext !== json) {
+      jsonPath = jsonPath.replace(ext, json);
+    }
+
+    return jsonPath;
   }
 }
