363cad99b8c41ce4ef230dde40382d1e5fde1111
Marc Mintel <marc@mintel.me>
2020-06-11 22:13:21 +0700

    add e2e tests for app

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { BlueprintModule } from '../blueprint';
 import { RecordModule } from '../record';
 import { ConfigModule } from '../config'
+import { AppController } from './app.controller';
 import { Options } from '..';
 
 @Module({
@@ -14,9 +15,14 @@ import { Options } from '..';
     BlueprintModule,
     RecordModule
   ],
+  controllers: [
+    AppController
+  ]
 })
 export class AppModule {
   public static register(options: Options): DynamicModule {
+    if (!options) throw new Error('Please provide options.');
+
     return {
       module: AppModule,
       global: true,
--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,24 +1,60 @@
+import path from 'path';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from '../src/app/app.module';
+import { AppModule } from '../src/app';
 
-describe('AppController (e2e)', () => {
+const options = {
+  blueprintsPath: path.resolve('../example/data/blueprints'),
+  recordsPath: path.resolve('../example/data/content')
+}
+
+describe('AppModule (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule.register(options)],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  test('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Here will be my amazing react frontend');
+  });
+
+  test('/graphql (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/graphql')
+      .set('Accept', 'text/html')
+      .expect(200)
+  });
+
+  test('blueprints (Query)', () => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        operationName: null,
+        variables: {},
+        query: `
+          query {
+            blueprints {
+              title
+            }
+          }
+        `,
+      })
+      .expect(200)
+      .expect(({ body }) => {
+        const blueprints = body.data.blueprints;
+        expect(blueprints.length).toBeGreaterThan(0);
+        expect(blueprints).toContainEqual({ title: 'Document' });
+        expect(blueprints).toContainEqual({ title: 'Collections' });
+      })
   });
 });
